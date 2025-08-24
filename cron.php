e221e35f79d6b819ccf2034a2b807e7c00337766
Arkdevlarry <akereleayomide106@gmail.com>
2025-08-24 20:07:21 +0100

    Puch it

---
+++ cron.php
@@ -0,0 +1,10 @@
+<?php
+
+// cron.php in public_html
+chdir(__DIR__);
+require 'vendor/autoload.php';
+
+$app = require_once __DIR__.'/bootstrap/app.php';
+$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
+
+$kernel->call('schedule:run');
