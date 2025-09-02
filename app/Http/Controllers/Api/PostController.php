431169030ea7028375c0ff06e5525fd11c739572
Arkdevlarry <akereleayomide106@gmail.com>
2025-09-02 12:54:09 +0100

    Payment and CMS

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -51,6 +51,7 @@ class PostController extends Controller
         $posts = $query->paginate($perPage);
         $categories = PostCategory::orderBy('name')->get(["id","name","slug"]);
         // $tags = Post::pluck('tags')->flatten()->unique()->values();
+        sleep(10);
         return response()->json([
             'message' => 'Posts retrieved successfully.',
             'posts' => $posts,
