302a0da822806786e06b9bf37102ff017d2b2392
Akerele Quadri <akereleayomide106@gmail.com>
2025-09-03 11:04:21 +0100

    test blog

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -1,5 +1,5 @@
 <template>
-  <DefaultLayout>
+  <BlogLayout>
     <div class="font-serif text-gray-900">
       <div id="app" class="font-sans antialiased text-gray-800 bg-gray-100 min-h-screen">
         <div class="container mx-auto p-4 md:p-8">
@@ -146,13 +146,13 @@
         </div>
       </div>
     </div>
-  </DefaultLayout>
+  </BlogLayout>
 </template>
 
 <script setup>
 import { ref, onMounted } from "vue";
 import { usePostStore } from "@/stores/posts";
-import DefaultLayout from "@/layouts/DefaultLayout.vue";
+import BlogLayout from "@/layouts/BlogLayout.vue";
 import SkeletonCardBlog from "@/components/common/SkeletonCardBlog.vue";
 import { formatDate } from "@/utils/helpers";
 import { IMG_URL } from "@/utils/urls";
