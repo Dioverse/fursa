c5564b9a77219a2d55af89aca16e166dd91bbf2e
Arkdevlarry <akereleayomide106@gmail.com>
2025-09-30 20:39:30 +0100

    Allow

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -19,7 +19,7 @@ class ProductController extends Controller
     public function index(Request $request): JsonResponse
     {
         // Start building the query with eager loading for the category
-        $query = Product::with(['category:id,name,slug', 'images:id,product_id,path', 'discount:value,type']);
+        $query = Product::with(['category:id,name,slug', 'images:id,product_id,path', 'discount:product_id,value,type']);
 
         // --- Filtering Options ---
         // Filter by category
