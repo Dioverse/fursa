b51ba6783d57f907781a1140a092ef1ce5f6d97e
DEOVAZE LTD <153617776+Dioverse@users.noreply.github.com>
2025-10-05 21:28:56 +0100

    prepare merge with AY

--- src/router/index.js
+++ src/router/index.js
@@ -16,7 +16,6 @@ const AddressesView = () => import('@/views/dashboard/AddressesView.vue')
 const WishlistView = () => import('@/views/dashboard/WishlistView.vue')
 const ShopView = () => import('@/views/shop/ShopView.vue')
 const ProductDetailView = () => import('@/views/shop/ProductDetailView.vue')
-const Categories = () => import('@/views/shop/Categories.vue')
 const CategoryView = () => import('@/views/shop/CategoryView.vue')
 const CartView = () => import('@/views/cart/CartView.vue')
 const CheckoutView = () => import('@/views/cart/CheckoutView.vue')
@@ -88,12 +87,6 @@ const router = createRouter({
       name: 'shop',
       component: ShopView,
       meta: { title: 'Shop' },
-    },
-    {
-      path: '/categories',
-      name: 'categories',
-      component: Categories,
-      meta: { title: 'Categories'},
     },
     {
       path: '/shop/category/:slug',
